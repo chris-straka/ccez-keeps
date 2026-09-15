830af20b5e081be6b4f7eac68fb928bea6a6752e
chris-straka <45607257+chris-straka@users.noreply.github.com>
2026-09-15 15:56:32 -0600

    Fix RemindersUiTest import

--- android/app/src/androidTest/java/dev/cstraka/keeps/ui/RemindersUiTest.kt
+++ android/app/src/androidTest/java/dev/cstraka/keeps/ui/RemindersUiTest.kt
@@ -2,6 +2,7 @@ package dev.cstraka.keeps.ui
 
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import androidx.test.ext.junit.runners.AndroidJUnit4
